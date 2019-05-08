4f5af0e778501a260103785a49edf42406b76c9b
Aku Kangas <aku.kangas@metropolia.fi>
2019-05-09 01:45:33 +0300

    unuset functions removed

--- client/src/components/Main/Main.css.js
+++ client/src/components/Main/Main.css.js
@@ -1,10 +1,11 @@
-import { drawerWidth } from 'styles/vars.css'
+import { drawerWidth, headerHeight } from 'styles/vars.css'
 export default theme => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing.unit * 2,
     marginTop: theme.spacing.unit * 8,
     width:'100%',
+    minHeight:`calc(100vh - ${headerHeight}px)`,
     [theme.breakpoints.up('sm')]: {
       width: `calc(100% - ${drawerWidth}px)`,
     },
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -2,30 +2,35 @@ import React, { useState, useEffect } from 'react';
 import cl from 'classnames';
 import { Paper } from '@material-ui/core';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { List, ListSubheader, } from '@material-ui/core';
-import FeedItem from 'components/FeedItem'
+import { List, Fade } from '@material-ui/core';
+import FeedItem from 'components/FeedItem';
 import http from 'utils/http';
 import Reader from 'components/Reader';
 
-function Main({ classes, match, history }) {
+function Main({ classes, match, history, setCategory }) {
   const [reader, setReader] = useState(null);
   const [feed, setFeed] = useState({ docs: [] });
   const url = match.params.category ? `/${match.params.category}` : '/';
-  const getFirstPage = () => http({ url }).then(setFeed)
 
-  useEffect(() => { 
-    http({ 
-      url: match.params.category ? `/${match.params.category}` : '/'
-    })
-      .then(setFeed)
-    }, [match.params.category]);
+  useEffect(() => {
+    const { category } = match.params;
+    setCategory(category)
+    http({
+      url: category ? `/${category}` : '/',
+    }).then(feed => {
+      setFeed(feed);
+      window.scrollTo(0, 0);
+    });
+  }, [match.params.category]);
 
-  const getNext = () => http({ url, params: { page: feed.page + 1 } }).then(nextPage => setFeed({ ...nextPage, docs: [...feed.docs, ...nextPage.docs] }))
+  const getNext = () =>
+    http({ url, params: { page: feed.page + 1 } }).then(nextPage =>
+      setFeed({ ...nextPage, docs: [...feed.docs, ...nextPage.docs] })
+    );
 
   return (
     <Paper component="main" className={cl(classes.root)}>
       <List className={cl(classes.list)}>
-        <ListSubheader>{match.params.category}</ListSubheader>
         <InfiniteScroll
           dataLength={feed.docs.length} //This is important field to render the next data
           next={getNext}
@@ -33,20 +38,17 @@ function Main({ classes, match, history }) {
           loader={'Loading...'}
           endMessage={'Yay! You have seen it all'}
           children={feed.docs}
-          // below props only if you need pull down functionality
-          refreshFunction={getFirstPage}
-          pullDownToRefresh
-          pullDownToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>}
-          releaseToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>}
         >
-          {feed.docs.map(item => <FeedItem key={item.guid} item={item} />)}
+          {feed.docs.map((item, index) => (
+            <Fade in={feed.docs.length > 0} timeout={{ enter: 350, exit: 350 }}>
+              <FeedItem key={item.guid} item={item} />
+            </Fade>
+          ))}
         </InfiniteScroll>
       </List>
       <Reader url={reader} setReader={setReader} />
     </Paper>
   );
 }
-
-
 
 export default Main;
