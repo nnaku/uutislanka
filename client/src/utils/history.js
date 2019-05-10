f84f4ad398819269ef7b9c87bf9d52d26383dda1
Aku Kangas <aku.kangas@metropolia.fi>
2019-05-11 00:18:39 +0300

    google analytics mode to history listener

--- client/src/utils/history.js
+++ client/src/utils/history.js
@@ -1,3 +1,8 @@
 import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-139977545-1');
 const history = createBrowserHistory();
+const unlisten = history.listen((location, action) => ( 
+  history.action === 'PUSH' && ReactGA.pageview(location.pathname + location.search)
+  ))
 export default history
