d31ed65c93bb3117b3b378ffb6d25af92ad2b9ab
Aku Kangas <aku.kangas@metropolia.fi>
2019-05-11 01:03:53 +0300

    ga dev added

--- client/src/utils/history.js
+++ client/src/utils/history.js
@@ -1,8 +1,16 @@
 import { createBrowserHistory } from 'history';
 import ReactGA from 'react-ga';
-ReactGA.initialize('UA-139977545-1');
 const history = createBrowserHistory();
-const unlisten = history.listen((location, action) => ( 
+const dev = 'UA-139977545-2'
+const heroku = 'UA-139977545-1'
+
+if (process.env.NODE_ENV === 'development') {
+  ReactGA.initialize(dev, { debug: true, })
+} else {
+  ReactGA.initialize(heroku)
+}
+
+const unlisten = history.listen((location, action) => (
   history.action === 'PUSH' && ReactGA.pageview(location.pathname + location.search)
-  ))
+))
 export default history
